c189e4d895704dadd73643dac2f7dea950be847e
Andie Similon <andie.similon@ap.be>
2026-03-18 14:51:37 +0000

    opdracht added

--- slopify_starter/index.ts
+++ slopify_starter/index.ts
@@ -12,6 +12,7 @@ app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set('views', path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.set("port", process.env.PORT || /* istanbul ignore next */ 3000);
 
